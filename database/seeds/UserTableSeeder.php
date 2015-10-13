2241f83177fef3beab5c0e31831de859e814fe70
Krit <kritcg@gmail.com>
2015-10-13 20:28:18 +0700

    Add User and Content seeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class, 2)->create()->each(function($u){
+            $u->contents()->save(factory(App\Content::class)->make());
+        });
+    }
+}
