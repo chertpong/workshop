337cba1f77ef1e6cfdf983390ae49b672b44d6b7
Krit <kritcg@gmail.com>
2015-10-24 22:48:33 +0700

    Add admin account for dev

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,6 +11,13 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
+        $user = new \App\User();
+        $user->name = 'admin';
+        $user->email = 'admin@admin.com';
+        $user->password = bcrypt('admin');
+        $user->remember_token = str_random(10);
+        $user->save();
+
         factory(App\User::class, 2)->create()->each(function($u){
             $u->contents()->save(factory(App\Content::class)->make());
         });
