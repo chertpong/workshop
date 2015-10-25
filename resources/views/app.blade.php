aa00223b9b02ae2d9aa198e1042e3b3f7d91c867
Krit <kritcg@gmail.com>
2015-10-25 14:20:21 +0700

    add "pictures" to navigator

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,6 +36,9 @@
             <ul class="nav navbar-nav">
                 <li><a href="{{ url('/') }}">Home</a></li>
                 <li><a href="{{ url('/contents') }}">Contents</a></li>
+                @if (Auth::check())
+                <li><a href="{{ url('/pictures') }}">Pictures</a></li>
+                @endif
             </ul>
 
             <ul class="nav navbar-nav navbar-right">
