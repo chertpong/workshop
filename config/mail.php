463019cac76c3757a3f40a3f5428d1edac3de67f
Krit <kritcg@gmail.com>
2015-10-25 21:15:56 +0700

    config -password reset function -email (mailgun)

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['no-reply@kritacademy.com' => null, 'name' => 'admin'],
 
     /*
     |--------------------------------------------------------------------------
