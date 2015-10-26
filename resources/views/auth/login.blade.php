5bcf64c0ad426e6003c570e85ff3bf0d64c74e70
caramaii <caramaii@hotmail.com>
2015-10-26 21:27:11 +0700

    Margin Top

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,11 @@
 @extends('app')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
+    <div class="container-fluid" id="loginbg">
+        <div class="row" id="loginrow">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Login</div>
+                    <div class="panel-heading" id="panellogin">Login</div>
                     <div class="panel-body">
                         @if (count($errors) > 0)
                             <div class="alert alert-danger">
