e3ca76b9d31f7a6aee2578159a827bc48c67f10d
Krit <kritcg@gmail.com>
2015-10-27 00:34:18 +0700

    Fix Content's create.blade.php transparent bg +panel

--- resources/views/contents/create.blade.php
+++ resources/views/contents/create.blade.php
@@ -16,29 +16,20 @@
                     </div>
                 @endif
                 </div>
-                    <div class="panel-body">
-                        <div class="form-group">
-                            <label for="title">Title of content</label>
-                            <input type="title" class="form-control" id="title" placeholder="Title" name="title" ng-model="title">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Post new content</div>
+                        <div class="panel-body">
+                            <div class="form-group">
+                                <label for="title">Title of content</label>
+                                <input type="title" class="form-control" id="title" placeholder="Title" name="title" ng-model="title">
+                            </div>
+                            <br>
+                            <div text-angular name="htmlcontent" ng-model="htmlcontent"></div>
+                            <h3>Raw HTML in a text area</h3>
+                            <textarea class="form-control col-xs-12" rows="10" ng-model="htmlcontent"></textarea>
+                            <button class="btn btn-success pull-right" ng-click="save()">Create</button>
                         </div>
-                        <br>
-                        <div text-angular name="htmlcontent" ng-model="htmlcontent"></div>
-                        <h3>Raw HTML in a text area</h3>
-                        <textarea class="form-control col-xs-12" rows="10" ng-model="htmlcontent"></textarea>
-                        <button class="btn btn-success pull-right" ng-click="save()">Create</button>
                     </div>
-                {{--<form class="form-horizontal" method="POST" action={{url('contents/store')}}>--}}
-                    {{--<input type="hidden" name="_token" value="{{ csrf_token() }}">--}}
-                    {{--<div class="form-group">--}}
-                        {{--<label for="title">Title of content</label>--}}
-                        {{--<input type="title" class="form-control" id="title" placeholder="Title" name="title">--}}
-                    {{--</div>--}}
-                    {{--<div class="form-group">--}}
-                        {{--<label for="content">Content</label>--}}
-                        {{--<textarea class="form-control" id="content" rows="10" name="content" placeholder="Content.."></textarea>--}}
-                    {{--</div>--}}
-                    {{--<button type="submit" class="btn btn-success pull-right">Create</button>--}}
-                {{--</form>--}}
             </div>
         </div>
     </div>
