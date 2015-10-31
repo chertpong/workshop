5da804b97528665e982ceb795e964b3f9e7b5c1b
caramaii <caramaii@hotmail.com>
2015-11-01 00:46:19 +0700

    CSS

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,51 +19,39 @@
     @include('script')
 </head>
 <style>
-    @font-face {
-        font-family: lucien;
-        src: {!! url("bower_components/bootstrap/fonts/DRdeco.ttf") !!};
-    }
+
 </style>
 <body ng-app="myApp">
 <div id="body-wrapper">
-<nav id="navigation" class="nav navbar-fixed-top navbar-default navbar-trans" role="navigation">
-    <div id="headnav" class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#head-nav" aria-expanded="false">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand hvr-underline-from-left" href="{{ url('/') }}" id="home"><strong>Home</strong></a>
-        </div>
-        <div class="navbar-collapse collapse" id="head-nav">
-            <ul class="nav navbar-nav navbar-left" id="menu2">
-                <li id="contentb" class="hvr-underline-from-left"><a href="{{ url('/contents') }}"><strong>Content</strong></a></li>
-                <li id="contactb"><a class="hvr-underline-from-left" href="{{ url('/#history') }}"><strong>History</strong></a></li>
+    <nav class="nav navbar-fixed-top">
+        <div id='cssmenu'>
+            <ul id="menubar">
+                <li class='active'><a id="button0" href="{{ url('/') }}">Home</a></li>
+                <li><a id="button1" href=#history>History</a></li>
+                <li ><a id="button2" href="{{ url('/contents') }}">Content</a></li>
+                <li ><a id="button3" href=#contact>Contact</a></li>
+                @if(Auth::check())
+                    <ul class="nav navbar-nav">
+                        <li><a href={{url('contents/create')}} class="hvr-underline-from-left">Create content</a></li>
+                        <li><a href={{url('pictures')}} class="hvr-underline-from-left">Manage pictures</a></li>
+                    </ul>
+                @endif
+                @if (Auth::guest())
+                    <li id="button4" class="pull-right"><a  id=button4" href="{{ url('/auth/login') }}">Sign in</a></li>
+                    {{--<li><a href={{url('auth/register')}}>Register</a></li>--}}
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle hvr-underline-from-left" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href={{url('auth/logout')}}>Logout</a></li>
+                        </ul>
+                    </li>
+                @endif
             </ul>
-            <ul class="nav navbar-nav navbar-right" id="signin">
-					@if(Auth::check())
-						<ul class="nav navbar-nav">
-							<li><a href={{url('contents/create')}} class="hvr-underline-from-left">Create content</a></li>
-                            <li><a href={{url('pictures')}} class="hvr-underline-from-left">Manage pictures</a></li>
-						</ul>
-					@endif
-					@if (Auth::guest())
-						<li><a href="{{ url('/auth/login') }}" class="bsignin hvr-underline-from-left" id="sign-in"><strong>Sign in</strong></a></li>
-						{{--<li><a href={{url('auth/register')}}>Register</a></li>--}}
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle hvr-underline-from-left" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href={{url('auth/logout')}}>Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
+
         </div>
-    </div>
-</nav>
+    </nav>
+
 
 @yield('content')
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,64 +1,92 @@
 @extends('app')
 
 @section('content')
-    <div class="row" id="mainrow">
+    <div class="row " id="slidediv">
         <section class="container-fluid" id="sec1">
-            <div id="slideshow">
-                <div>
-                    <img  src="https://c2.staticflickr.com/6/5655/22360803525_2fe4d688d8_b.jpg" class="img-responsive">
-                </div>
-                <div>
-                    <img src="https://c1.staticflickr.com/1/580/22371477131_0090674b52_b.jpg" class="img-responsive">
-                </div>
-                <div>
-                    <img src="https://c2.staticflickr.com/6/5795/22360807025_f943722364_b.jpg" class="img-responsive">
-                </div>
-                <div>
-                    <img src="https://c1.staticflickr.com/1/629/21738039214_f98f5f4bd7_b.jpg" class="img-responsive">
-                </div>
-                <div>
-                    <img src="https://c2.staticflickr.com/6/5703/22334806556_df71d062ab_b.jpg" class="img-responsive">
-                </div>
-                <div>
-                    <img src="https://c2.staticflickr.com/6/5802/22360840685_21f548b534_b.jpg" class="img-responsive">
+            <div id="carousel" class="carousel slide carousel-fade col-md-offset-3 fit" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel" data-slide-to="1"></li>
+                    <li data-target="#carousel" data-slide-to="2"></li>
+                    <li data-target="#carousel" data-slide-to="3"></li>
+                    <li data-target="#carousel" data-slide-to="4"></li>
+                    <li data-target="#carousel" data-slide-to="5"></li>
+                </ol>
+                <div class="carousel-inner brownshadow">
+                    <div class="active item">
+                        <img class="img-responsive" src="https://c2.staticflickr.com/6/5655/22360803525_2fe4d688d8_b.jpg">
+                    </div>
+                    <div class="item">
+                        <img class="img-responsive" src="https://c1.staticflickr.com/1/580/22371477131_0090674b52_b.jpg">
+                    </div>
+                    <div class="item">
+                        <img class="img-responsive" src="https://c2.staticflickr.com/6/5795/22360807025_f943722364_b.jpg">
+                    </div>
+                    <div class="item">
+                        <img class="img-responsive" src="https://c1.staticflickr.com/1/629/21738039214_f98f5f4bd7_b.jpg">
+                    </div>
+                    <div class="item">
+                        <img class="img-responsive" src="https://c2.staticflickr.com/6/5703/22334806556_df71d062ab_b.jpg">
+                    </div>
+                    <div class="item">
+                        <img class="img-responsive" src="https://c2.staticflickr.com/6/5802/22360840685_21f548b534_b.jpg">
+                    </div>
                 </div>
             </div>
         </section>
 
-       <div class="row">
-           <div id="temple-name" class="col-xs-12 col-md-6 col-md-offset-3">
-               <h1 class="tlt">
-                   <ul class="texts">
-                       <li>วัดเจดีย์หลวง</li>
-                       <li>Wat Chedi Luang</li>
-                   </ul>
-               </h1>
-           </div>
-       </div>
+
+        <h1 class="tlt">
+            <ul class="texts">
+                <li>วัดเจดีย์หลวง</li>
+                <li>Wat Chedi Luang</li>
+            </ul>
+        </h1>
 
     </div>
+
     <section id="history">
-            <h1 id="history-head">History
-                <ul class="nav navbar-nav navbar-right" id="clang">
-                    <li><button class="button button-circle button-tran hvr-fade" onclick="changeLanguageToEn()">TH</button></li>
-                    <li><button class="button button-circle button-tran hvr-fade" onclick="changeLanguageToTh()">EN</button></li>
-                </ul>
-            </h1>
-            <div class="temple-history">
-                <p id="th-his">วัดเจย์ดีหลวง หรือ "วัดโชติการาม" หรือ "ราชกุฏคาร" ก็เรียกสร้างในสมัยพระเจ้าแสนเมืองรัชกาลที่ 7 แห่งราชวงศ์มังราย(ครองราชย์ พ.ศ. 1929-1944) ไม่ปรากฎปีที่สร้างวัดแน่ชัด
-                    วัดเจย์ดีหลวงเป็นพระอารามหลวงแบบโบราณที่แบ่งเขต พุทธาวาส/สังฆวาสอีก 4 ที่ตั้งรายล้อมเขตพุทธาวาสอยู่ทั้ง 4 ทิศ กิจวัตรประจำวัน เช่น ไหว้พระสวดมนต์แต่ละสังฆวาสหรือคณะ ต่างคนต่างทำในวิหารของตน
-                    ถ้าเป็นงานสำคัญๆจึงจะร่วมกันในพระวิหารหลวงและประกอบสังฆกรรม อันเป็นสัฆกิจร่วมกันในพระอุโบสถ ซึ่งมีอยู่แห่งเดียวในเขตพุทธาวาสที่ได้ชื่อว่า "วัดเจย์ดีหลวง" ตั้งแต่โบราณกาลมาเพราะถือเอาพระธาตุเจดีย์หลวงเป็นเนมิตกนามกล่าวได้ว่าเป็นเจติย
-                    สถานโบราณสูงใหญ่ที่สุดของไทย<br>
-                    <br>
-                    &nbsp;&nbsp;&nbsp;&nbsp;ช่วงพุทธศักราช 2471-2481 ถือได้ว่าเป็นทศวรรษการปรับปรุงเปลี่ยนแปลง เพื่อสร้างสรรค์พัฒนาใหม่ในทุกๆด้านของวัดเจดีย์หลวงทำการรื้อถอนสิ่งปรักหักพัง แล้วถางป่าที่ขึ้นคลุมเสนาสนะโบราณสถานต่างๆออก
-                    แล้วสร้างเสริมเสนาสนะใหม่ให้เป็นวัดสมบูรณ์แบบในเวลาต่อมา จนมีพระสงฆ์สามเณรอยู่ประจำปีละมากๆหลังจากถูกทิ้งร้างมานานและจัดตั้งสำนักเรียน พระปริยัติธรรมขึ้นให้เป็นศูนย์กลางการศึกษาปริยัติศึกษาแผนใหม่ในจังหวัดเชียงใหม่
-                    ด้วยการมองการณ์ไกล และผลงานที่เป็นรูปธรรมดังกล่าวจึงทำให้วัดเจดีย์หลวงได้เลื่อฐานะขึ้นเป็น "พระอารามหลวงชั้นตรี" ชนิด "วรวิหาร" เป็น "วัดเจดีย์หลวงวรวิหาร" เมื่อปีพุทธศักราช 2481 เป็นต้นมา
-                </p>
+        <div class="row" id="history-div">
+            <div class="container" >
+                <h1 id="historyhead">History
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><button class="button button-circle button-tran hvr-fade" onclick="changeth()">TH</button></li>
+                        <li><button class="button button-circle button-tran hvr-fade" onclick="changeen()">EN</button></li>
+                    </ul>
+                </h1>
+                <hr>
+                <div class="templehistory">
+                    <p id="thhis">วัดเจย์ดีหลวง หรือ "วัดโชติการาม" หรือ "ราชกุฏคาร" ก็เรียกสร้างในสมัยพระเจ้าแสนเมืองรัชกาลที่ 7 แห่งราชวงศ์มังราย(ครองราชย์ พ.ศ. 1929-1944) ไม่ปรากฎปีที่สร้างวัดแน่ชัด
+                        วัดเจย์ดีหลวงเป็นพระอารามหลวงแบบโบราณที่แบ่งเขต พุทธาวาส/สังฆวาสอีก 4 ที่ตั้งรายล้อมเขตพุทธาวาสอยู่ทั้ง 4 ทิศ กิจวัตรประจำวัน เช่น ไหว้พระสวดมนต์แต่ละสังฆวาสหรือคณะ ต่างคนต่างทำในวิหารของตน
+                        ถ้าเป็นงานสำคัญๆจึงจะร่วมกันในพระวิหารหลวงและประกอบสังฆกรรม อันเป็นสัฆกิจร่วมกันในพระอุโบสถ ซึ่งมีอยู่แห่งเดียวในเขตพุทธาวาสที่ได้ชื่อว่า "วัดเจย์ดีหลวง" ตั้งแต่โบราณกาลมาเพราะถือเอาพระธาตุเจดีย์หลวงเป็นเนมิตกนามกล่าวได้ว่าเป็นเจติย
+                        สถานโบราณสูงใหญ่ที่สุดของไทย<br>
+                        <br>
+                        &nbsp;&nbsp;&nbsp;&nbsp;ช่วงพุทธศักราช 2471-2481 ถือได้ว่าเป็นทศวรรษการปรับปรุงเปลี่ยนแปลง เพื่อสร้างสรรค์พัฒนาใหม่ในทุกๆด้านของวัดเจดีย์หลวงทำการรื้อถอนสิ่งปรักหักพัง แล้วถางป่าที่ขึ้นคลุมเสนาสนะโบราณสถานต่างๆออก
+                        แล้วสร้างเสริมเสนาสนะใหม่ให้เป็นวัดสมบูรณ์แบบในเวลาต่อมา จนมีพระสงฆ์สามเณรอยู่ประจำปีละมากๆหลังจากถูกทิ้งร้างมานานและจัดตั้งสำนักเรียน พระปริยัติธรรมขึ้นให้เป็นศูนย์กลางการศึกษาปริยัติศึกษาแผนใหม่ในจังหวัดเชียงใหม่
+                        ด้วยการมองการณ์ไกล และผลงานที่เป็นรูปธรรมดังกล่าวจึงทำให้วัดเจดีย์หลวงได้เลื่อฐานะขึ้นเป็น "พระอารามหลวงชั้นตรี" ชนิด "วรวิหาร" เป็น "วัดเจดีย์หลวงวรวิหาร" เมื่อปีพุทธศักราช 2481 เป็นต้นมา
+                    </p>
 
-                <p id="en-his">It was in May, 1932 that i had the aus
+                    <p id="enhis">It was in May , 1932 that I had the auspicious occasion to be ordained as a novice
+                        at Wat Chedi Luang when I was approaching the age of 15. The reason is that four persons of my age,
+                        who were also my relatives at Muang District, Lamphun Province, had come to stay at Wat Chedi Luang
+                        before I came to reside here, the were determined to ordain as novices before the rain retreat,
+                        In the meantime, Mr.Saengthong, my older brother, who was doing some business in the city, learned
+                        that my relatives were about to be ordained as a novice, He wanted me, to resort to monastive life
+                        so that I could study Thai language, It was good luck for me to have such an opportunity, without
+                        which I would have goine somewhere else.
 
-                </p>
+                        Upon first entering the temple, the thing that attracted my eye and impressed me was
+                        grand ruined pagoda.The condition of the pagoda as i saw it in 1932 is the same even now as i am writing
+                        this article(1990).It is wonderful that during the span of 58 years Chedi Luang or the grand pagoda is
+                        still in the same condition,not more ruined despite undergoing several earthquqkes, especially the 2 strong
+                        earthquakes of September 1989. The reliquaty could survive such incident.It seemed to be a miracle, It should be acknowledge
+                        that a certain group of people in Chiang Mai being aware of its significance,met together and decided to do restorations,
+                        Cooperation was expressed by the Sangha Order,government allocated restoration budgets for three successive
+                        years from 1990 through 1992
+                    </p>
+                </div>
             </div>
+        </div>
     </section>
 
     <section id="headcontent">
